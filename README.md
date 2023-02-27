ad0c0f74260508a0c0be34a648e2215d908fcc89
Tertius <11148488+TertiusL@users.noreply.github.com>
2023-02-27 10:24:52 +0200

    Final commit
    I have reformatted the look of the page a bit, and added support for localstorage to remember the users favourites

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### About this project
+```
+This project is made as a training and demonstration excercise. 
+It uses the browsers local storage to store the users shows marked as favourites, so they can be accessed even after the browser is closed.
+I also used axios as my api handler.
--- src/components/MovieBuff.vue
+++ src/components/MovieBuff.vue
@@ -4,28 +4,56 @@
     <p>
       Type your desired movie or series name below to search, or take a look at your favourites!
     </p>
-    <div>
+    <div style="padding-bottom: 10px;">
       <input type="text" v-model="searchText" placeholder="Search...">
       <button @click="Search">Search</button>
       <button @click="Favourites">Favourites</button>
     </div>
-    <div class="search-results">
-      <ul>
-        <li v-for="result in searchResult" :key="result.imdbID">
-          <div>
-            <div id="imageSize">
-              <img alt="Poster" :src="result.Poster" id="logo">
-            </div>
-            <div>
-              <p>{{ result.Title }} ({{ result.Year }})</p>
-              <p>IMDB ID: {{ result.imdbID }}</p>
-              <p>Type: {{ result.Type.charAt(0).toUpperCase() + result.Type.slice(1) }}</p>
-              <button v-if="!isFavourited(result)" @click="Favourite(result)">Favourite</button>
-              <button v-else @click="Unfavourite(result)">Unfavourite</button>             
-            </div>
-          </div>
-        </li>
-      </ul>
+    <div class="main">
+      <table v-if="!viewFavourites">
+        <tbody>
+          <tr v-for="result in searchResult" :key="result.imdbID">
+            <td>
+              <div class="result-item">
+                <div style="text-align: center;">
+                  <img alt="Poster" :src="result.Poster" id="poster">
+                </div>                
+              </div>
+            </td>
+            <td>
+              <div class="details">
+                  <p>{{ result.Title }} ({{ result.Year }})</p>
+                  <p>IMDB ID: {{ result.imdbID }}</p>
+                  <p>Type: {{ result.Type.charAt(0).toUpperCase() + result.Type.slice(1) }}</p>
+                  <button v-if="!isFavourited(result)" @click="Favourite(result)">Favourite</button>
+                  <button v-else @click="Unfavourite(result)">Unfavourite</button>             
+                </div>
+            </td>            
+          </tr>
+        </tbody>
+      </table>
+      <table v-if="viewFavourites">
+        <tbody>
+          <tr v-for="result in favourites" :key="result.imdbID">
+            <td>
+              <div class="result-item">
+                <div style="text-align: center;">
+                  <img alt="Poster" :src="result.Poster" id="poster">
+                </div>                
+              </div>
+            </td>
+            <td>
+              <div class="details">
+                  <p>{{ result.Title }} ({{ result.Year }})</p>
+                  <p>IMDB ID: {{ result.imdbID }}</p>
+                  <p>Type: {{ result.Type.charAt(0).toUpperCase() + result.Type.slice(1) }}</p>
+                  <button v-if="!isFavourited(result)" @click="Favourite(result)">Favourite</button>
+                  <button v-else @click="Unfavourite(result)">Unfavourite</button>             
+                </div>
+            </td>            
+          </tr>
+        </tbody>
+      </table>
     </div>
   </container>
 </template>
@@ -44,11 +72,27 @@ export default {
       searchResult: [],
       apiUrl: 'https://www.omdbapi.com/?s=',
       apiKey: '&apikey=3bef232e&',
-      favourites: []
+      favourites: [],
+      viewFavourites: false
+    }
+  },
+  created(){
+    const favourites = localStorage.getItem('favourites')
+    console.log('Created: '+ favourites)
+    if (favourites) {
+      this.favourites = JSON.parse(favourites)
+      console.log('Created if: '+ this.favourites)
+    }
+  },
+  watch: {
+    favourites: function(newFavourites) {
+      localStorage.setItem('favourites', JSON.stringify(newFavourites))
+      console.log('watch: '+ newFavourites)
     }
   },
   methods: {
     async Search() {
+      this.viewFavourites = false
       try{
         const response = await axios.get(this.apiUrl + this.searchText + this.apiKey)
         this.searchResult = response.data.Search;   
@@ -68,7 +112,7 @@ export default {
       return this.favourites.some(favourite => favourite.imdbID === result.imdbID)
     },
     Favourites() {
-      this.searchResult = this.favourites
+      this.viewFavourites = true
     }
   }
 }
@@ -94,9 +138,40 @@ a {
   display: flex;
   flex-wrap: wrap;
 }
-
+.main {
+  display: flex;
+  justify-content: center;
+}
 .search-results li {
   flex-basis: calc(33.33% - 10px);
   margin: 5px;
+}
+.result-item {
+  align-items: center;
+  margin-bottom: 10px;
+}
+#poster{
+  width: 50%;
+  height: 50%;
+}
+.poster img {
+  max-width: 100%;
+}
+
+.details {
+  flex: 1;
+}
+table {
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid rgb(83, 82, 82);
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #acaaaa;
 }
 </style>
